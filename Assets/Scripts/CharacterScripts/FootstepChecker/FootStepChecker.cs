7289c3943712a3b5576a6b25a0d5fbe92fc8d646
Romeo IV <94739286+Quattrus@users.noreply.github.com>
2022-09-02 19:06:23 +0800

    Update FootStepChecker.cs

--- Assets/Scripts/CharacterScripts/FootstepChecker/FootStepChecker.cs
+++ Assets/Scripts/CharacterScripts/FootstepChecker/FootStepChecker.cs
@@ -7,24 +7,26 @@ public class FootStepChecker : MonoBehaviour
     [SerializeField] GameObject player;
 
 
+
+
+
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.tag == "Grass")
-        {
-            player.gameObject.GetComponent<FootstepManager>().PlaySteps(0);
-        }
-        else if(collision.gameObject.tag == "Cement")
-        {
-            player.gameObject.GetComponent<FootstepManager>().PlaySteps(1);
-        }
-        else if(collision.gameObject.tag == "Cave")
-        {
-            player.gameObject.GetComponent<FootstepManager>().PlaySteps(2);
-        }
-        else if(collision.gameObject.tag == "Water")
-        {
-            player.gameObject.GetComponent<FootstepManager>().PlaySteps(3);
-        }
-       
+            if (collision.gameObject.tag == "Grass")
+            {
+                player.gameObject.GetComponent<FootstepManager>().PlaySteps(0);
+            }
+            else if (collision.gameObject.tag == "Cement")
+            {
+                player.gameObject.GetComponent<FootstepManager>().PlaySteps(1);
+            }
+            else if (collision.gameObject.tag == "Cave")
+            {
+                player.gameObject.GetComponent<FootstepManager>().PlaySteps(2);
+            }
+            else if (collision.gameObject.tag == "Water")
+            {
+                player.gameObject.GetComponent<FootstepManager>().PlaySteps(3);
+            }     
     }
 }
