d94e14b413ccd07e50a586d93d7c2f487392c47f
Romeo IV <94739286+Quattrus@users.noreply.github.com>
2022-09-02 17:40:00 +0800

    added colliders to play the footstep instead

---
+++ Assets/Scripts/CharacterScripts/FootstepChecker/FootStepChecker.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootStepChecker : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Grass")
+        {
+            player.gameObject.GetComponent<FootstepManager>().PlaySteps(0);
+        }
+        else if(collision.gameObject.tag == "Cement")
+        {
+            player.gameObject.GetComponent<FootstepManager>().PlaySteps(1);
+        }
+        else if(collision.gameObject.tag == "Cave")
+        {
+            player.gameObject.GetComponent<FootstepManager>().PlaySteps(2);
+        }
+        else if(collision.gameObject.tag == "Water")
+        {
+            player.gameObject.GetComponent<FootstepManager>().PlaySteps(3);
+        }
+       
+    }
+}
--- Assets/Scripts/CharacterScripts/FootstepManager.cs
+++ Assets/Scripts/CharacterScripts/FootstepManager.cs
@@ -8,11 +8,10 @@ public class FootstepManager : MonoBehaviour
     [SerializeField] List<AudioClip> waterSteps = new List<AudioClip>();
     [SerializeField] List<AudioClip> caveSteps = new List<AudioClip>();
     [SerializeField] List<AudioClip> cementSteps = new List<AudioClip>();
-    private Animator animator;
 
 
-    private enum Surface { grass, water, cave, cement};
-    private Surface surface;
+    //private enum Surface { grass, water, cave, cement};
+    //private Surface surface;
 
     private List<AudioClip> currentList;
 
@@ -21,89 +20,35 @@ public class FootstepManager : MonoBehaviour
     private void Start()
     {
         source = GetComponent<AudioSource>();
-        animator = GetComponent<Animator>();
     }
 
-    public void PlayStep(int stepType)
+    public void PlayFootstep()
     {
         AudioClip clip = currentList[Random.Range(0, currentList.Count)];
-        if(stepType > 1)
+        source.PlayOneShot(clip);
+    }
+
+    public void PlaySteps(int stepType)
+    {
+        if(stepType == 0)
         {
-            if(animator.GetFloat("MoveY") == 2)
-            {
-                source.PlayOneShot(clip);
-            }
-            
+            currentList = grassSteps;
+            PlayFootstep();
         }
         else if(stepType == 1)
         {
-            if(animator.GetFloat("MoveY") > 0.6f)
-            {
-                source.PlayOneShot(clip);
-            }
-            else if(animator.GetFloat("MoveY") < -0.6f)
-            {
-                source.PlayOneShot(clip);
-            }
-            
-
+            currentList = cementSteps;
+            PlayFootstep();
         }
-        else if(stepType < 1)
+        else if(stepType == 2)
         {
-            if(animator.GetFloat("MoveX") < -0.8f)
-            {
-                source.PlayOneShot(clip);
-            }
-            else if(animator.GetFloat("MoveX") > 0.8f)
-            {
-                source.PlayOneShot(clip);
-            }
-            
+            currentList = caveSteps;
+            PlayFootstep();
         }
-
-    }
-
-    private void SelectStepList()
-    {
-        switch (surface)
+        else if(stepType == 3)
         {
-            case Surface.grass:
-                currentList = grassSteps;
-                break;
-            case Surface.water:
-                currentList = waterSteps;
-                break;
-            case Surface.cave:
-                currentList = caveSteps;
-                break;
-            case Surface.cement:
-                currentList = cementSteps;
-                break;
+            currentList = waterSteps;
+            PlayFootstep();
         }
-    }
-
-    private void OnControllerColliderHit(ControllerColliderHit hit)
-    {
-        if (hit.transform.tag == "Grass")
-        {
-            surface = Surface.grass;
-        }
-
-        if (hit.transform.tag == "Water")
-        {
-            surface = Surface.water;
-        }
-
-        if (hit.transform.tag == "Cave")
-        {
-            surface = Surface.cave;
-        }
-        if(hit.transform.tag == "Cement")
-        {
-            surface = Surface.cement;
-        }
-
-        SelectStepList();
-
     }
 }
