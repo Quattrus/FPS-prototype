44db6e3779ae45cb1dfc624c83791dff35935ff4
Romeo IV <94739286+Quattrus@users.noreply.github.com>
2022-09-13 01:29:49 +0800

    Added Health Bar
    added health bar

---
+++ Assets/Scripts/Player/PlayerStatus/HealthController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HealthController : MonoBehaviour
+{
+    [Header("Health Main Parameters")]
+    [SerializeField] float playerHealth = 100f;
+    [SerializeField] float maxHealth = 100f;
+
+    [Header("Health Value Parameters")]
+    [SerializeField] float healingValue;
+
+
+    [Header("Health UI Elements")]
+    [SerializeField] Image healthBarUI = null;
+    [SerializeField] CanvasGroup healthSliderCanvasGroup = null;
+
+    private PlayerStateMachine playerStateMachine;
+
+
+    private void Awake()
+    {
+        playerStateMachine = GetComponent<PlayerStateMachine>();
+    }
+
+    private void Update()
+    {
+        healthBarUI.fillAmount = playerHealth / maxHealth;
+    }
+
+    public void DamagePlayer(int damageAmount)
+    {
+        playerHealth -= damageAmount;
+        healthBarUI.fillAmount = playerHealth / damageAmount;
+    }
+    public void PlayerHeal(int healAmount)
+    {
+        playerHealth += healAmount;
+        healthBarUI.fillAmount = playerHealth / maxHealth;
+    }    
+    
+}
